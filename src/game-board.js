bad2c7257ecb3fa1dbb1fc753186884e0e189ece
fredieposh <maor1234@gmail.com>
2025-09-27 19:01:18 +0300

    Enable adding ship to board

--- src/game-board.js
+++ src/game-board.js
@@ -13,12 +13,27 @@ class GameBoard {
         return this.#board;
     };
 
-    placeShipOnBoard(ship) {
+    placeShipOnBoard(ship, row, column, isHorizontal = true) {
         const shipSize = ship.getShipSize();
-        for(let i = 0; i < shipSize; i++) {
-            this.#board[1][i] = 1;
-        }
+        
+        if(isHorizontal) {
+            this.#placeShipHorizontally(shipSize, row, column);
+        } else {
+            this.#placeShipVertically(shipSize, row, column);
+        };
 
-        return this.#board;
+        return {board: this.#board,};
+    };
+
+    #placeShipHorizontally(shipSize, row, column) {
+        for(let i = 0; i < shipSize; i++) {
+            this.#board[row][i + column] = 1;
+        }
+    };
+
+    #placeShipVertically(shipSize, row, column) {
+        for(let i = 0; i < shipSize; i++) {
+            this.#board[i + row][column] = 1;
+        }
     };
 };
