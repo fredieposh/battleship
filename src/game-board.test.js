6ec27b58f03e7ee8562351bc4aa39f1c9f0b994d
fredieposh <maor1234@gmail.com>
2025-09-27 19:01:53 +0300

    Add testing to adding ship ability

--- src/game-board.test.js
+++ src/game-board.test.js
@@ -2,14 +2,33 @@ import {Ship} from './ship.js'
 import {GameBoard} from './game-board.js'
 
 describe('check placeShipOnBoard function', () => {
-    test('check placeShipOnBoard works', () => {
+    test('check placeShipOnBoard works for one ship horizontal placement', () => {
         const gameBoard = new GameBoard();
         const ship = new Ship('Destroyer');
-        const shipSize = ship.getShipSize();
 
-        gameBoard.placeShipOnBoard(ship);
-        for(let i = 0; i < shipSize; i++) {
-            expect(gameBoard.getBoard()[1][i]).toBe(1);
-        }
+        const changedGameBoared =  gameBoard.placeShipOnBoard(ship, 2, 5, true).board;
+        expect(changedGameBoared[2][5]).toBe(1);
+        expect(changedGameBoared[2][6]).toBe(1);
+
+    });
+
+    test('check placeShipOnBoard works for one ship vertical placement', () => {
+        const gameBoard = new GameBoard();
+        const ship = new Ship('Destroyer');
+
+        const changedGameBoared =  gameBoard.placeShipOnBoard(ship, 2, 5, false).board;
+        expect(changedGameBoared[2][5]).toBe(1);
+        expect(changedGameBoared[3][5]).toBe(1);
+
+    });
+
+    test('check placeShipOnBoard works for one ship vertical placement', () => {
+        const gameBoard = new GameBoard();
+        const ship = new Ship('Destroyer');
+
+        const changedGameBoared =  gameBoard.placeShipOnBoard(ship, 2, 5, false).board;
+        expect(changedGameBoared[2][5]).toBe(1);
+        expect(changedGameBoared[3][5]).toBe(1);
+
     });
 });
