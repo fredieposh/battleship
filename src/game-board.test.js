6145aa42efbd223ac5d5cc213b3aa300ee6c1958
fredieposh <maor1234@gmail.com>
2025-09-29 23:33:34 +0300

    Test placement of ship on another ship validation

--- src/game-board.test.js
+++ src/game-board.test.js
@@ -1,8 +1,8 @@
 import {Ship} from './ship.js'
 import {GameBoard} from './game-board.js'
 
-describe('check placeShipOnBoard function', () => {
-    test('check placeShipOnBoard works for one ship horizontal placement', () => {
+describe('test placeShipOnBoard function', () => {
+    test('test placeShipOnBoard works for one ship horizontal placement', () => {
         const gameBoard = new GameBoard();
         const ship = new Ship('Destroyer');
 
@@ -12,7 +12,7 @@ describe('check placeShipOnBoard function', () => {
 
     });
 
-    test('check placeShipOnBoard works for one ship vertical placement', () => {
+    test('test placeShipOnBoard works for one ship vertical placement', () => {
         const gameBoard = new GameBoard();
         const ship = new Ship('Destroyer');
 
@@ -22,7 +22,7 @@ describe('check placeShipOnBoard function', () => {
 
     });
 
-    test('check placeShipOnBoard works for one ship vertical placement', () => {
+    test('test placeShipOnBoard works for one ship vertical placement', () => {
         const gameBoard = new GameBoard();
         const ship = new Ship('Destroyer');
 
@@ -30,5 +30,27 @@ describe('check placeShipOnBoard function', () => {
         expect(changedGameBoared[2][5]).toBe(1);
         expect(changedGameBoared[3][5]).toBe(1);
 
+    });
+
+    test('test if placing the first coordinate out of bounds returns warning 1', () => {
+        const gameBoard = new GameBoard();
+        const ship = new Ship('Destroyer');
+
+        expect(gameBoard.placeShipOnBoard(ship, 0, 10, false)).toBe("Placing coordinates have to be within the board boundries.");
+    });
+
+    test('test if placing the first coordinate out of bounds returns warning 2', () => {
+        const gameBoard = new GameBoard();
+        const ship = new Ship('Destroyer');
+
+        expect(gameBoard.placeShipOnBoard(ship, 10, 0, false)).toBe("Placing coordinates have to be within the board boundries.");
+    });
+
+    test('test that placing a ship on another ship returns error', () => {
+        const gameBoard = new GameBoard();
+        const ship1 = new Ship('Destroyer');
+        const ship2 = new Ship('Destroyer');
+        gameBoard.placeShipOnBoard(ship1, 1, 1, false);
+        expect(gameBoard.placeShipOnBoard(ship2, 1, 0, true)).toBe("Can\'t place ship on another ship.");
     });
 });
