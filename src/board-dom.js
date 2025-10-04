6172b1f436f9c07bdf15961c6918cdcee8dfaa6c
fredieposh <maor1234@gmail.com>
2025-10-04 18:17:09 +0300

    Add another board to the page layout

--- src/board-dom.js
+++ src/board-dom.js
@@ -1,21 +1,28 @@
-export {createBoardDom};
+export {loadBoards};
 
-const boardDom = document.querySelector('#board-container');
+const humanUser = 'human';
+const computerUser = 'computer';
+const humanBoardDom = document.querySelector('#human-board-container');
+const computerBoardDom = document.querySelector('#computer-board-container');
 
-function createBoardDom() {
+function loadBoards() {
+    createBoardDom(humanUser, humanBoardDom );
+    createBoardDom(computerUser, computerBoardDom );
+};
+
+function createBoardDom(user, container) {
     for(let i = 0; i < 10; i++) {
         for(let j = 0; j < 10; j ++) {
-            const newTile = createBoardTileDiv(i, j);
-            boardDom.appendChild(newTile);
+            const newTile = createBoardTileDiv(user ,i, j);
+            container.appendChild(newTile);
         };
     };
-    console.log(boardDom);
-}
+};
 
-function createBoardTileDiv(row, column) {
+function createBoardTileDiv(user, row, column) {
     const newTile = document.createElement('div');
     newTile.classList.add('board-tile');
-    newTile.setAttribute('id',`${row}-${column}`);
+    newTile.setAttribute('id',`${user}-${row}-${column}`);
 
     return newTile;
-}
+};
