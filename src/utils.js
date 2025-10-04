d6e22fbf0fafdf901b1f0ed279e6c25fb5e4a774
fredieposh <maor1234@gmail.com>
2025-10-04 21:36:43 +0300

    Add functions to utils module
    Those two functions will help placing the computer ships on the board.
    For now it helps us test the gameplay.

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,16 @@
-export {copyObj};
+export {copyObj, coinFlip, drawNumner};
 
 
 function copyObj(obj) {
     const objCopy = {};
     Object.entries(obj).forEach(([key, value]) => objCopy[key] = value);
     return objCopy;
+}
+
+function coinFlip() {
+    return Math.random() > 0.5;
+}
+
+function drawNumner() {
+    return Math.floor(Math.random() * 10);
 }
