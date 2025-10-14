d0e17555c159a56067ea4bef466a66688e3bfa0d
fredieposh <maor1234@gmail.com>
2025-10-14 19:20:58 +0300

    hitInDirection

--- src/computer-algo.js
+++ src/computer-algo.js
@@ -1,4 +1,3 @@
-import {GameBoard} from './game-board.js'
 export {Hit, HitTracker};
 
 class Hit {
@@ -20,6 +19,7 @@ class HitTracker {
     direction;
     shipType;
     probeDirections;
+    nextCoord;
 
     constructor(firstHit, shipType, direction = null) {
         this.firstHit = firstHit;
@@ -31,12 +31,13 @@ class HitTracker {
             [1, 0],
             [-1, 0]
         ]; 
+        this.nextCoord = null;
     }
 
     probeForHit(board) {
         
         const {index, direction} = this.#directionDraw(this.probeDirections);
-        const hitCoord = [this.firstHit.hitLocation[0] + direction[0], this.firstHit.hitLocation[0] + direction[1]];
+        const hitCoord = [this.firstHit.hitLocation[0] + direction[0], this.firstHit.hitLocation[1] + direction[1]];
         const hitResult = board.receiveAttack(hitCoord[0], hitCoord[1]);
 
         if (hitResult === 'A hit coordinates have to be within the board boundries.') {
@@ -56,11 +57,8 @@ class HitTracker {
 
         if (hitResult.hitShipType === this.shipType) {
             this.#loadDirections(this.#returnDirections(direction));
-            this.firstHit.nextHit = new Hit(hitCoord, hitResult.hitShipType, null, this.firstHit); 
             return hitResult.hitShipType;
         };
-
-
     };
 
     #directionDraw(array) {
@@ -81,6 +79,66 @@ class HitTracker {
     #loadDirections(directionsArray) {
         this.direction = [];
         directionsArray.forEach(direction => this.direction.push(direction));
+    };
+
+    hitInDirection(hit, board) {
+        const direction = this.direction[0];
+        
+        if(hit === null) {
+            if(this.nextCoord === null) {
+                this.nextCoord = this.#assembleCoord(this.firstHit.hitLocation, direction);
+            } else {
+                this.nextCoord = this.#assembleCoord(this.nextCoord, direction);
+            };
+
+            if (board.getBoard()[this.nextCoord[0]][this.nextCoord[1]] === 'x') {
+                newHit = new Hit(this.nextCoord, this.shipType);
+                return newHit;
+            };
+
+            const hitResults = board.receiveAttack(this.nextCoord[0], this.nextCoord[1]);
+            
+            if (hitResults === 'A hit coordinates have to be within the board boundries.') {
+                this.direction.splice(0, 1);
+                this.nextCoord = null;
+                newHit = new Hit(this.nextCoord, hitResults);
+                return newHit;
+            };
+
+            if (hitResults === 'x') {
+                this.direction.splice(0, 1);
+                this.nextCoord = null;
+                newHit = new Hit(this.nextCoord, hitResults);
+                return newHit;
+            };
+
+            if (hitResults.hitShipType !== this.shipType) {
+                this.direction.splice(0, 1);
+                this.nextCoord = null;
+                newHit = new Hit(this.nextCoord, hitResults.hitShipType);
+                return newHit;
+            };
+
+            if (hitResults.hitShipType === this.shipType) {
+                newHit = new Hit(this.nextCoord, hitResults.hitShipType);
+                return newHit;
+            };
+
+        };
+
+        if(this.direction.length === 1) {
+            hit.prevHit = this.hitInDirection(hit.prevHit, board);
+        };
+
+        if(this.direction.length > 1) {
+            hit.nextHit = this.hitInDirection(hit.nextHit, board);
+        };
+    };
+
+    #assembleCoord(arr1, arr2) {
+        const firstCoord = arr1[0] + arr2[0];
+        const secondCoord = arr1[1] + arr2[1];
+
+        return [firstCoord, secondCoord];
     }
 };
- 
--- src/computer-algo.test.js
+++ src/computer-algo.test.js
@@ -18,7 +18,6 @@ describe('Test probeForHit function', () => {
             probeHit = tracker.probeForHit(board);
         } while (probeHit !== 'Destroyer');
 
-        expect(tracker.firstHit.nextHit.hitLocation).toStrictEqual([1,2]);
         expect(tracker.direction).toStrictEqual( [[0, 1], [0, -1]] );
     });
 
@@ -37,7 +36,115 @@ describe('Test probeForHit function', () => {
             probeHit = tracker.probeForHit(board);
         } while (probeHit !== 'Destroyer');
 
-        expect(tracker.firstHit.nextHit.hitLocation).toStrictEqual([2,1]);
         expect(tracker.direction).toStrictEqual( [[1, 0], [-1, 0]] );
     });    
 });
+
+
+describe('Test hitInDirection function', () => {
+    test('Test hitInDirection with one single horizontal ship - Hit is at one of the ship edges', () => {
+        const ship = new Ship('Cruiser');
+        const board = new GameBoard();
+        let probeHit;
+
+        board.placeShipOnBoard(ship, 1, 1, true);
+        board.receiveAttack(1, 1);
+
+        const firstHit = new Hit([1,1], 'Cruiser');
+        const tracker = new HitTracker(firstHit, firstHit.hitType);
+
+        do {
+            probeHit = tracker.probeForHit(board);
+        } while (probeHit !== 'Cruiser');
+
+        const direction = tracker.direction;
+        while(
+            board.getBoard()[1][1] != 'x' ||
+            board.getBoard()[1][2] != 'x' ||
+            board.getBoard()[1][3] != 'x'
+        ) {
+            tracker.hitInDirection(tracker.firstHit, board);
+        };
+
+    });
+
+    test('Test hitInDirection with one single horizontal ship - Hit is on the other edge', () => {
+        const ship = new Ship('Cruiser');
+        const board = new GameBoard();
+        let probeHit;
+
+        board.placeShipOnBoard(ship, 1, 1, true);
+        board.receiveAttack(1, 2);
+
+        const firstHit = new Hit([1,2], 'Cruiser');
+        const tracker = new HitTracker(firstHit, firstHit.hitType);
+
+        do {
+            probeHit = tracker.probeForHit(board);
+        } while (probeHit !== 'Cruiser');
+
+        while(
+            board.getBoard()[1][1] != 'x' ||
+            board.getBoard()[1][2] != 'x' ||
+            board.getBoard()[1][3] != 'x'
+        ) {
+            tracker.hitInDirection(tracker.firstHit, board);
+        };
+
+    });
+
+    test('Test hitInDirection with one single horizontal ship - Hit is at the center of the ship', () => {
+        const ship = new Ship('Cruiser');
+        const board = new GameBoard();
+        let probeHit;
+
+        board.placeShipOnBoard(ship, 1, 1, true);
+        board.receiveAttack(1, 2);
+
+        const firstHit = new Hit([1,2], 'Cruiser');
+        const tracker = new HitTracker(firstHit, firstHit.hitType);
+
+        do {
+            probeHit = tracker.probeForHit(board);
+        } while (probeHit !== 'Cruiser');
+
+        let counter = 0;
+        while(
+            (board.getBoard()[1][1] != 'x' ||
+            board.getBoard()[1][2] != 'x' ||
+            board.getBoard()[1][3] != 'x' ) &&
+            counter < 3000
+        ) {
+            tracker.hitInDirection(tracker.firstHit, board);
+            counter ++;
+        };
+
+    });
+
+    test('Test hitInDirection with one single horizontal ship - Ship is at the edge of the board', () => {
+        const ship = new Ship('Cruiser');
+        const board = new GameBoard();
+        let probeHit;
+
+        board.placeShipOnBoard(ship, 1, 0, true);
+        board.receiveAttack(1, 0);
+
+        const firstHit = new Hit([1,0], 'Cruiser');
+        const tracker = new HitTracker(firstHit, firstHit.hitType);
+
+        do {
+            probeHit = tracker.probeForHit(board);
+        } while (probeHit !== 'Cruiser');
+
+        while(
+            board.getBoard()[1][0] != 'x' ||
+            board.getBoard()[1][1] != 'x' ||
+            board.getBoard()[1][2] != 'x'
+        ) {
+            tracker.hitInDirection(tracker.firstHit, board);
+        };
+        console.log(board.getBoard())
+    });
+
+});
+
